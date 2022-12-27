d7845d762bb5b8784f646c99dd1c2f3bdf7a52d8
Fausio Matsinhe <fausio.matsinhe@kooli.io>
2022-12-27 15:19:00 +0200

    router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Route, RouterModule    } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListComponent } from './employee/list/list.component';
 import { NavMenuComponent } from './nav/nav-menu/nav-menu.component';
+import { CreateComponent } from './employee/create/create.component';
+
+const appRouts: Route[] = [
+  {path: 'employee-list' , component: ListComponent},
+  {path: 'employee-create', component: CreateComponent},
+  {path: '', redirectTo:'employee-list', pathMatch: 'full'}
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     ListComponent,
-    NavMenuComponent
+    NavMenuComponent,
+    CreateComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRouts),
   ],
   providers: [],
   bootstrap: [AppComponent]
